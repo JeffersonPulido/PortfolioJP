58ca37604c15212506b37ccf635d858dcc8994a2
JeffersonPulido <jeffersonpulido55@gmail.com>
2022-12-27 15:49:35 -0500

    Navbar v1.3 Finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,14 +10,14 @@ export const Navbar = () => {
       <nav class='navbar navbar-expand-lg navbar-light bg-danger sticky-top'>
         <div class='container'>
           <a class='navbar-brand text-white' href='#'>
-            <img src={logo} alt="Logo" class='logo' />
+            <img src={logo} alt="Logo" class='logo d-flex img-fluid' />
           </a>
           <button class='btn btn-danger navbar-toggler border-3 px-2 text-white' type='button' data-bs-toggle='offcanvas' data-bs-target='#offcanvasExample' aria-controls='offcanvasExample'>
             <i class="fa-solid fa-ellipsis-vertical"></i>
           </button>
           <div class='offcanvas offcanvas-start-lg bg-danger' tabindex='-1' id='offcanvasExample' aria-labelledby='offcanvasExampleLabel'>
             <div class='offcanvas-header d-flex d-lg-none'>
-              <h5 class='offcanvas-title text-white' id='offcanvasExampleLabel'>Navbar</h5>
+              <h5 class='offcanvas-title text-white' id='offcanvasExampleLabel'>JPulido Dev</h5>
               <a href='javascript:void(0) ' class='text-reset p-0' data-bs-dismiss='offcanvas' aria-label='close'>
                 <img src={logoClose} alt="Icon Close Navbar" />
               </a>
@@ -31,7 +31,13 @@ export const Navbar = () => {
                   <a class='nav-link active' aria-current='page' href='#'>About</a>
                 </li>
                 <li class='nav-item'>
+                  <a class='nav-link active' aria-current='page' href='#'>Projects</a>
+                </li>
+                <li class='nav-item'>
                   <a class='nav-link active' aria-current='page' href='#'>Skills</a>
+                </li>
+                <li class='nav-item'>
+                  <a class='nav-link active' aria-current='page' href='#'>Testimonials</a>
                 </li>
                 <li class='nav-item'>
                   <a class='nav-link active' aria-current='page' href='#'>Contact</a>
