ce4cc6426478cc96ee3513db0e7d08de27bef3c4
JeffersonPulido <jeffersonpulido55@gmail.com>
2022-12-27 16:31:42 -0500

    Header v1.3 Finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
       <div className={style.container}>
         <div className={style.containerName}>
           <h1 className={style.title}>Hi, I'm Jefferson Pulido</h1>
-          <h2 className={style.descripcion}>Developer FrontEnd</h2>
+          <h2 className={style.descripcion}>Developer Web FrontEnd</h2>
           <a className={style.btn_neon} href="https://jeffersonpulido-cv.netlify.app/" target="_blank">Ver CV</a>
         </div>
       </div>
