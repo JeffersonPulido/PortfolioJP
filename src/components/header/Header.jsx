cf7af980cacc36c503b72e611a37b9eeb17f999d
JeffersonPulido <jeffersonpulido55@gmail.com>
2022-12-27 16:02:39 -0500

    Header v1.2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import style from './header.module.css'
-// import Logo from '../../assets/images/logo_single.png'
-// <img src={Logo} alt="LogoJP" className={style.imgLogo} />
+
+
 export const Header = () => {
   return (
     <>
-        <div className={style.container}>
-            <div className={style.containerName}>
-                <h1 className={style.title}>Hi, I'm Jefferson Pulido</h1>
-                <h2 className={style.descripcion}>Developer FrontEnd</h2>
-            </div>
+      <div className={style.container}>
+        <div className={style.containerName}>
+          <h1 className={style.title}>Hi, I'm Jefferson Pulido</h1>
+          <h2 className={style.descripcion}>Developer FrontEnd</h2>
+          <a className={style.btn_neon} href="https://jeffersonpulido-cv.netlify.app/" target="_blank">Ver CV</a>
         </div>
+      </div>
     </>
   )
 }
