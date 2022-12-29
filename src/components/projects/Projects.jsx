e6ece60540457dcd7a39caddd95dcee644f83a0d
JeffersonPulido <jeffersonpulido55@gmail.com>
2022-12-29 11:10:04 -0500

    Project v1.1

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,14 +1,193 @@
 import React from 'react'
+import caratula from '../../assets/images/Caratula1.webp'
 import './Projects.css'
 
 export const Projects = () => {
   return (
     <>
-        <div className='containerProject'>
-            <div className='containerTitle'>
-                <h1>PROJECTS</h1>
-            </div>
+      <div className='containerProject'>
+        <div className='containerTitleProject'>
+          <h1>PROJECTS</h1>
         </div>
+        <div className='containerInfoProject'>
+          <div className='containerCard'>
+            <div className='cardThumb'>
+              <img src={caratula} alt="" />
+            </div>
+            <div className='cardTitle'>
+              <h1>Proyecto</h1>
+            </div>
+            <div className='cardDesc'>
+              <p>Sistema de control de productos, sedes, proveedores y ventas para supermercado desarrollado en la MisionTIC & Universidad El Bosque</p>
+            </div>
+            <div className='cardTech'>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVA</li>
+              </ul>
+            </div>
+            <div className='cardBtn'>
+              <a href="">Deploy</a>
+              <a href="">Github</a>
+            </div>
+          </div>
+          <div className='containerCard'>
+            <div className='cardThumb'>
+              <img src={caratula} alt="" />
+            </div>
+            <div className='cardTitle'>
+              <h1>Proyecto</h1>
+            </div>
+            <div className='cardDesc'>
+              <p>Sistema de control de productos, sedes, proveedores y ventas para supermercado desarrollado en la MisionTIC & Universidad El Bosque</p>
+            </div>
+            <div className='cardTech'>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVA</li>
+              </ul>
+            </div>
+            <div className='cardBtn'>
+              <a href="">Deploy</a>
+              <a href="">Github</a>
+            </div>
+          </div>
+          <div className='containerCard'>
+            <div className='cardThumb'>
+              <img src={caratula} alt="" />
+            </div>
+            <div className='cardTitle'>
+              <h1>Proyecto</h1>
+            </div>
+            <div className='cardDesc'>
+              <p>Sistema de control de productos, sedes, proveedores y ventas para supermercado desarrollado en la MisionTIC & Universidad El Bosque</p>
+            </div>
+            <div className='cardTech'>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVA</li>
+              </ul>
+            </div>
+            <div className='cardBtn'>
+              <a href="">Deploy</a>
+              <a href="">Github</a>
+            </div>
+          </div>
+          <div className='containerCard'>
+            <div className='cardThumb'>
+              <img src={caratula} alt="" />
+            </div>
+            <div className='cardTitle'>
+              <h1>Proyecto</h1>
+            </div>
+            <div className='cardDesc'>
+              <p>Sistema de control de productos, sedes, proveedores y ventas para supermercado desarrollado en la MisionTIC & Universidad El Bosque</p>
+            </div>
+            <div className='cardTech'>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVA</li>
+              </ul>
+            </div>
+            <div className='cardBtn'>
+              <a href="">Deploy</a>
+              <a href="">Github</a>
+            </div>
+          </div>
+          <div className='containerCard'>
+            <div className='cardThumb'>
+              <img src={caratula} alt="" />
+            </div>
+            <div className='cardTitle'>
+              <h1>Proyecto</h1>
+            </div>
+            <div className='cardDesc'>
+              <p>Sistema de control de productos, sedes, proveedores y ventas para supermercado desarrollado en la MisionTIC & Universidad El Bosque</p>
+            </div>
+            <div className='cardTech'>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVA</li>
+              </ul>
+            </div>
+            <div className='cardBtn'>
+              <a href="">Deploy</a>
+              <a href="">Github</a>
+            </div>
+          </div>
+          <div className='containerCard'>
+            <div className='cardThumb'>
+              <img src={caratula} alt="" />
+            </div>
+            <div className='cardTitle'>
+              <h1>Proyecto</h1>
+            </div>
+            <div className='cardDesc'>
+              <p>Sistema de control de productos, sedes, proveedores y ventas para supermercado desarrollado en la MisionTIC & Universidad El Bosque</p>
+            </div>
+            <div className='cardTech'>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVA</li>
+              </ul>
+            </div>
+            <div className='cardBtn'>
+              <a href="">Deploy</a>
+              <a href="">Github</a>
+            </div>
+          </div>
+          <div className='containerCard'>
+            <div className='cardThumb'>
+              <img src={caratula} alt="" />
+            </div>
+            <div className='cardTitle'>
+              <h1>Proyecto</h1>
+            </div>
+            <div className='cardDesc'>
+              <p>Sistema de control de productos, sedes, proveedores y ventas para supermercado desarrollado en la MisionTIC & Universidad El Bosque</p>
+            </div>
+            <div className='cardTech'>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVA</li>
+              </ul>
+            </div>
+            <div className='cardBtn'>
+              <a href="">Deploy</a>
+              <a href="">Github</a>
+            </div>
+          </div>
+          <div className='containerCard'>
+            <div className='cardThumb'>
+              <img src={caratula} alt="" />
+            </div>
+            <div className='cardTitle'>
+              <h1>Proyecto</h1>
+            </div>
+            <div className='cardDesc'>
+              <p>Sistema de control de productos, sedes, proveedores y ventas para supermercado desarrollado en la MisionTIC & Universidad El Bosque</p>
+            </div>
+            <div className='cardTech'>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVA</li>
+              </ul>
+            </div>
+            <div className='cardBtn'>
+              <a href="">Deploy</a>
+              <a href="">Github</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
